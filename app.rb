442518fcafaa6afdaf513dcdfe3b451c5652a2a4
GreenTer <thenewlife@tut.by>
2017-09-05 01:59:12 +0300

    Start L29+

--- app.rb
+++ app.rb
@@ -13,7 +13,32 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index
+end
+
+get	'/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@user_name = params[:user_name]
+	@user_phone = params[:user_phone]
+	@user_time = params[:user_time]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	c = Client.new
+	c.name = @user_name
+	c.phone = @user_phone
+	c.datastamp = @user_time
+	c.barber = @barber
+	c.color = @color
+	c.save
+
+	erb "Thx man!"
 end
